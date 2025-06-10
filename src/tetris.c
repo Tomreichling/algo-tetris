d2e6202046de2cd3b1c2a98078e43b1235ca2f35
loshido <loshido@pm.me>
2025-06-10 19:49:34 +0200

    descendre_piece

--- src/tetris.c
+++ src/tetris.c
@@ -1,9 +1,5 @@
 #include "./tetris.h"
 
-// Largeur et hauteur par defaut d'une image correspondant a nos criteres
-#define LargeurFenetre 1200
-#define HauteurFenetre 800
-
 /* La fonction de gestion des evenements, appelee automatiquement par le systeme
 des qu'une evenement survient */
 void gestionEvenement(EvenementGfx evenement);
@@ -12,19 +8,14 @@ int main(int argc, char **argv)
 {
     srand(time(NULL));
 	initialiseGfx(argc, argv);
-	
-	prepareFenetreGraphique("OpenGL", LargeurFenetre, HauteurFenetre);
-	
-	/* Lance la boucle qui aiguille les evenements sur la fonction gestionEvenement ci-apres,
-		qui elle-meme utilise fonctionAffichage ci-dessous */
+	prepareFenetreGraphique("OpenGL", LARGEUR, LONGUEUR);
 	lanceBoucleEvenements();
-	
 	return 0;
 }
 
+// Variable globale accessible dans tous le programme.
 Jeu jeu = {0};
-/* La fonction de gestion des evenements, appelee automatiquement par le systeme
-des qu'une evenement survient */
+
 void gestionEvenement(EvenementGfx evenement)
 {
 	static bool pleinEcran = false; // Pour savoir si on est en mode plein ecran ou pas
@@ -33,19 +24,16 @@ void gestionEvenement(EvenementGfx evenement)
             jeu.grille[i][j] = 0;
         }
     }
-	// ATTENTION PENSER A FREE LORSQUE L'ON QUITTE.
 
     jeu.etat = MENU;
 	static DonneesImageRGB *image = NULL;
-
 	
 	switch (evenement)
 	{
 		case Initialisation:
-			// Configure le systeme pour generer un message Temporisation
-			// toutes les 20 millisecondes
             image = lisBMPRGB("./assets/titre-tetris.bmp");
-			demandeTemporisation(20);
+
+			demandeTemporisation(1000);
 			jeu.etat = JEU;
 			break;
 		
@@ -55,22 +43,25 @@ void gestionEvenement(EvenementGfx evenement)
                 case MENU:
                     break;
                 case JEU:
-				
+                    // - Vérifier si on peut descendre la pièce actuelle
+                    // - Descendre la pièce actuelle si possible
+                    // - sinon placer la pièce dans la grille
+                    // - et prendre la suivante
+                    // - vérifier si des lignes peuvent se supprimer
+                    break;
                 case FIN:
-				break;
+				    break;
             }
 			
 			rafraichisFenetre();
 			break;
 			
-			case Affichage:
+		case Affichage:
             switch (jeu.etat)
             {
-
                 case MENU:
 					afficheMenu(image);
                     break;
-                
                 case JEU:
 					affichageJeu();
 					afficherCarreau(0, 0, 1);
@@ -81,127 +72,62 @@ void gestionEvenement(EvenementGfx evenement)
 					afficherTitre(16, 3);
                     break;
                 case FIN:
+                    // Afficher le jeu + Gameover + score
                     break;
             }
-			
-			// // On part d'un fond d'ecran blanc
-			// effaceFenetre (0, 0, 0);
-			
-			// // Affichage d'une ligne bleue
-			// couleurCourante(0, 0, 255);
-			// epaisseurDeTrait(10);
-			// ligne(0, 0, (largeurFenetre()-600)/2, (hauteurFenetre()-400)/2);
-
-			// // Affichage d'un rectangle "saumon"
-			// couleurCourante(255, 128, 128);
-			// rectangle((largeurFenetre()-600)/2, (hauteurFenetre()-400)/2, (largeurFenetre()-600)/2+600, (hauteurFenetre()-400)/2+400);
-
-			// // Affichage d'un texte vert sombre avec ombrage en police de taille 20
-			// // L'effet d'ombrage vient tout simplement du dessin du texte en gris clair
-			// // sous le texte voulu, avec un léger décalage
-			// couleurCourante(210, 210, 210);
-			// epaisseurDeTrait(3);
-			// afficheChaine("L'algo c'est rigolo", 20, 77, 13);
-			// couleurCourante(0, 128, 0);
-			// epaisseurDeTrait(2);
-			// afficheChaine("L'algo c'est rigolo", 20, 75, 15);
-
-			// couleurCourante(255, 0, 0);
 			break;
-			
 		case Clavier:
-			switch (caractereClavier())
-			{
-				case 'Q': /* Pour sortir quelque peu proprement du programme */
-				case 'q':
-					break;
-				case 'F':
-				case 'f':
-					pleinEcran = !pleinEcran; // Changement de mode plein ecran
-					if (pleinEcran)
-						modePleinEcran();
-					else
-						redimensionneFenetre(LargeurFenetre, HauteurFenetre);
-					break;
+            if(caractereClavier() == 'f') {
+                pleinEcran = !pleinEcran;
+                if (pleinEcran)
+                    modePleinEcran();
+                else
+                    redimensionneFenetre(LARGEUR, LONGUEUR);
+                break;
+            }
+            switch(jeu.etat) {
+                // *caractereClavier() donne la touche*
+                case MENU:
+                    switch (caractereClavier()){
+                        case 'q':
+                        case 'Q':
+                            // quitter
+                            break;
+                        case 32:
+                            jeu.etat = JEU;
+                            printf("Lancement du jeu\n");
+                            demandeTemporisation(1000);
+                            rafraichisFenetre();
+                            break;
+                    }
+                    // espace pour démarrer le jeu 
+                    // q pour quitter le jeu
+                    break;
+                case JEU:
+                    // p pour mettre le jeu en pause
+                    // q pour aller à gauche
+                    // d pour aller à droite
+                    // s pour aller en bas
+                    // e ou r pour tourner la pièce
+                    // espace pour sauter
+                    break;
+                case FIN:
+                    // q pour quitter le jeu
+                    break;
+            }
 
-				case 'R':
-				case 'r':
-					// Configure le systeme pour generer un message Temporisation
-					// toutes les 20 millisecondes (rapide)
-					demandeTemporisation(20);
-					break;
-
-				case 'L':
-				case 'l':
-					// Configure le systeme pour generer un message Temporisation
-					// toutes les 100 millisecondes (lent)
-					demandeTemporisation(100);
-					break;
-
-				case 'S':
-				case 's':
-					// Configure le systeme pour ne plus generer de message Temporisation
-					demandeTemporisation(-1);
-					break;
-				case 32 :
-					if (jeu.etat == MENU) {
-					jeu.etat = JEU;
-					printf("%d \n", jeu.etat);
-					}
-					else if (jeu.etat == JEU) {
-						printf("vous etes en jeu\n");
-					}
-					else {
-						jeu.etat = MENU;
-						printf("%d \n", jeu.etat);
-					}
-					break;
-			}
-			break;
-			
 		case ClavierSpecial:
-        
-			printf("ASCII %d\n", toucheClavier());
-			break;
-
-		case BoutonSouris:
-			switch (etatBoutonSouris())
-			{
-				case GaucheAppuye:
-					printf("Bouton gauche appuye en : (%d, %d)\n", abscisseSouris(), ordonneeSouris());
-					// Si le bouton gauche de la souris est appuye, faire repartir
-					// la balle de la souris
-					break;
-				case GaucheRelache:
-					printf("Bouton gauche relache en : (%d, %d)\n", abscisseSouris(), ordonneeSouris());
-					break;
-				case DroiteAppuye:
-				case DroiteRelache:
-					puts("Bouton droite");
-					break;
-				case MilieuAppuye:
-				case MilieuRelache:
-					puts("Bouton milieu");
-					break;
-				case ScrollDown:
-					puts("Scroll down");
-					break;
-				case ScrollUp:
-					puts("Scroll up");
-					break;
-				case ScrollRight:
-					puts("Scroll right");
-					break;
-				case ScrollLeft:
-					puts("Scroll left");
-					break;
-			}
-			break;
-		
-		case Souris: // Si la souris est deplacee
-			break;
-		
-		case Inactivite: // Quand aucun message n'est disponible
+            switch(jeu.etat) {
+                case JEU:
+                    // Flèche gauche -> aller à gauche
+                    // Flèche droite -> aller à droite
+                    // Flèche du bas -> aller en bas
+                    // Flèche du haut -> tourner la pièce
+                    break;
+                default:
+                    printf("ASCII %d\n", toucheClavier());
+                    break;
+            }
 			break;
 		
 		case Redimensionnement: // La taille de la fenetre a ete modifie ou on est passe en plein ecran
